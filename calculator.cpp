cb63c6a0f4d7c3c387dd08de3c4fbdb80077d5d7
Ananya212 <annroy.212@gmail.com>
2015-09-09 15:05:10 +0530

    this is clone of web app

--- calculator.cpp
+++ calculator.cpp
@@ -1,3 +1,4 @@
 hello this is my new file for today. I am bored and cant think of anything else to write so be satisfied with this alright.
 uffo..y so irritating
 enjoy!!!
+Hello everyone new day, new thing
